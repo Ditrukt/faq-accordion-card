a24f2aecec8aca31ea35cd21c2b7bd814d89b4ee
Ditrukt <lzadorinegorl@gmail.com>
2023-08-15 22:27:13 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var acc = document.querySelectorAll(".accordion");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      panel.style.padding = '0';
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.padding = '0 0 1rem 0';
+    }
+  });
+}
